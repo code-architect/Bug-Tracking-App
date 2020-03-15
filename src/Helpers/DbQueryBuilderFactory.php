9b00b71ba1f4cfd309f4642a093d8c3aa2af8f15
code-architect <codearchitectdeveloper@gmail.com>
2020-03-16 04:30:25 +0530

    Created a factory named DbQueryBuilderFactory

---
+++ src/Helpers/DbQueryBuilderFactory.php
@@ -0,0 +1,45 @@
+<?php
+namespace App\Helpers;
+
+use App\Database\MySQLiConnection;
+use App\Database\MySQLiQueryBuilder;
+use App\Database\PDOConnection;
+use App\Database\PDOQueryBuilder;
+use App\Database\QueryBuilder;
+use App\Exception\DatabaseConnectionException;
+use App\Exception\MissingArgumentException;
+use App\Exception\NotFoundException;
+
+class DbQueryBuilderFactory
+{
+    /**
+     * @param string $credentialFile
+     * @param string $connectionType
+     * @param array $options
+     * @return QueryBuilder
+     * @throws NotFoundException
+     * @throws DatabaseConnectionException
+     * @throws MissingArgumentException
+     */
+    public static function make(string $credentialFile = 'database',
+                                string $connectionType = 'pdo',
+                                array $options = []):QueryBuilder
+    {
+        $connection = null;
+        $credentials = array_merge(Config::get($credentialFile, $connectionType), $options);
+        switch ($connectionType)
+        {
+            case 'pdo':
+                $connection = (new PDOConnection($credentials))->connect();
+                return new PDOQueryBuilder($connection);
+                break;
+            case 'mysqli':
+                $connection = (new MySQLiConnection($credentials))->connect();
+                return new MySQLiQueryBuilder($connection);
+                break;
+            default:
+                throw new DatabaseConnectionException('Connection type is not recognized',
+                    ['type' => $connectionType]);
+        }
+    }
+}
